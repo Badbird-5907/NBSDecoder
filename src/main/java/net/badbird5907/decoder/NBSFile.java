d02f593591cf43602450708e25a15a4468594aed
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-16 23:01:04 -0400

    Update NBSFile.java

--- src/main/java/net/badbird5907/decoder/NBSFile.java
+++ src/main/java/net/badbird5907/decoder/NBSFile.java
@@ -12,36 +12,33 @@ import java.lang.reflect.Field;
 @Getter
 @Setter
 public class NBSFile {
-    private short length,height;
-    private String name,author,origAuthor,desc;
-    private short tempo;
-    private byte autosave,autosaveDuration,timeSig;
-    private int minsSpent,leftClicks,rightClicks,blocksAdded,blocksRemoved;
-    //private String schemName;
+	private short length,height;
+	private String name,author,origAuthor,desc;
+	private short tempo;
+	private byte autosave,autosaveDuration,timeSig;
+	private int minsSpent,leftClicks,rightClicks,blocksAdded,blocksRemoved;
+//	private String schemName;
+	private LittleEndianDataInputStream inputStream;
 
-    private LittleEndianDataInputStream inputStream;
-    @SneakyThrows
-    public NBSFile(InputStream is){
-        inputStream = new LittleEndianDataInputStream(is);
-        for (Field declaredField : this.getClass().getDeclaredFields()) {
-            System.out.println("Field: " + declaredField.getName() + " | " + declaredField.getType().getName());
-            if (declaredField.getType().isAssignableFrom(short.class))
-                declaredField.set(this,inputStream.readShort());
-            else if (declaredField.getType().isAssignableFrom(String.class))
-                try{
-                    declaredField.set(this,inputStream.readUTF());
-                } catch (Exception e) {
-                    System.err.println("Failed to read string for " + declaredField.getName() + ", default to \"unknown\"");
-                    declaredField.set(this,"Unknown");
-                }
-            else if (declaredField.getType().isAssignableFrom(byte.class))
-                declaredField.set(this,inputStream.readByte());
-            else if (declaredField.getType().isAssignableFrom(int.class))
-                declaredField.set(this,inputStream.readInt());
-            System.out.println(declaredField.get(this));
-        }
-        System.out.println("Length: " + length);
-        System.out.println("Name: " + name);
-        System.out.println("Tempo: " + tempo);
-    }
+	@SneakyThrows
+	public NBSFile(InputStream is) {
+		inputStream = new LittleEndianDataInputStream(is);
+		for (Field declaredField : this.getClass().getDeclaredFields()) {
+			System.out.println("Field: " + declaredField.getName() + " | " + declaredField.getType().getName());
+			if (declaredField.getType().isAssignableFrom(short.class)) declaredField.set(this, inputStream.readShort());
+			else if (declaredField.getType().isAssignableFrom(String.class))
+				try {
+					declaredField.set(this, inputStream.readUTF());
+				} catch (Exception e) {
+					System.err.println("Failed to read string for " + declaredField.getName() + "; defaulted to \"Unknown\"");
+					declaredField.set(this, "Unknown");
+				}
+			else if (declaredField.getType().isAssignableFrom(byte.class)) declaredField.set(this, inputStream.readByte());
+			else if (declaredField.getType().isAssignableFrom(int.class)) declaredField.set(this, inputStream.readInt());
+			System.out.println(declaredField.get(this));
+		}
+		System.out.println("Length: " + length);
+		System.out.println("Name: " + name);
+		System.out.println("Tempo: " + tempo);
+	}
 }
